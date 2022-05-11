0e5e9a1407ee147a014841e18ed646b16af12b97
Nilesh Aditya <nileshaditya2000@gmail.com>
2022-05-11 23:55:35 +0530

    Add:New feaetureu

--- public/food-website/js/index.js
+++ public/food-website/js/index.js
@@ -295,7 +295,7 @@ putMarker();
 let marker = L.marker([12.931475071111107, 77.67880147111109], {icon : myIcon2}).addTo(mymap);
 
 marker.bindPopup(`
-<strong>My Location</strong></br>
+  <strong>My Location</strong></br>
 `).openPopup();
 
 mark.push([12.931475071111107, 77.67880147111109]);
